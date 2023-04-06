4c310cae0263bb6205fd8ca5497d56026b59bcef
SashiFernandez <sasfernandez198@gmail.com>
2023-04-05 21:03:16 -0300

    Ingreso Scanner

--- src/proyectomentor2/ProyectoMentor2.java
+++ src/proyectomentor2/ProyectoMentor2.java
@@ -1,10 +1,12 @@
 package proyectomentor2;
 
+import java.util.Scanner;
+
 
 public class ProyectoMentor2 {
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
     }
     
 }
